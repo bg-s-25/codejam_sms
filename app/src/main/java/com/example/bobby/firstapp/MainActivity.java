64a95679e678f98c54bd9dceac1a10bfc8170696
bg-s-25 <hgeorgio@stevens.edu>
2017-10-13 14:23:31 -0400

    Simplify UI

--- app/src/main/java/com/example/bobby/firstapp/MainActivity.java
+++ app/src/main/java/com/example/bobby/firstapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.bobby.firstapp;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,39 +10,35 @@ import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bt1; Button bt3;
-    EditText et1;
+    Button bt1;
+    EditText etPhone, etMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = (EditText) findViewById(R.id.editText);
+        etPhone = (EditText) findViewById(R.id.editTextPhone);
+        etMessage = (EditText) findViewById(R.id.editTextMessage);
         bt1 = (Button) findViewById(R.id.button);
-        bt3 = (Button) findViewById(R.id.button3);
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Will now attempt to send an SMS", Toast.LENGTH_SHORT).show();
 
-                String phoneNum = et1.getText().toString();
-                sendSMS(phoneNum, "SMS Message");
+                String phoneNum = etPhone.getText().toString();
+                String message = etMessage.getText().toString();
+
+                sendSMS(phoneNum, message);
             }
         });
 
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Input Data button clicked
-                startActivity(new Intent(MainActivity.this, InputDataActivity.class));
-            }
-        });
     }
 
     // Send an SMS message
     private void sendSMS(String phoneNumber, String message) {
+        if (phoneNumber == "") {return;}
         try {
             SmsManager sms = SmsManager.getDefault();
             sms.sendTextMessage(phoneNumber, null, message, null, null);
