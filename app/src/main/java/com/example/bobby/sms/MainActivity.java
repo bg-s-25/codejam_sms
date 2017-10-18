0e33a000722eca0a12a3fa7815ad0c19aa97098d
Justin John <justinjohn12356@gmail.com>
2017-10-17 20:12:24 -0700

    Here is the code

--- app/src/main/java/com/example/bobby/sms/MainActivity.java
+++ app/src/main/java/com/example/bobby/sms/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     Button bt2;
     static final int DATE_DIALOG_ID = 1;
     static final int TIME_DIALOG_ID = 2;
+    final Calendar cal = Calendar.getInstance();
     int year_x, month_x, date_x, hour_x, minute_x; // variables for year, month, day of month, hour, and minute set by user
     String dateStr, timeStr;
     String[] saveData;
@@ -38,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         etMessage = (EditText) findViewById(R.id.editTextMessage);
         bt1 = (Button) findViewById(R.id.button);
         bt2 = (Button) findViewById(R.id.buttonSetDateTime);
-        final Calendar cal = Calendar.getInstance();
         year_x = cal.get(Calendar.YEAR);
         month_x = cal.get(Calendar.MONTH);
         date_x = cal.get(Calendar.DAY_OF_MONTH);
@@ -84,14 +84,26 @@ public class MainActivity extends AppCompatActivity {
         if (phoneNumber == "") {return;}
         try {
             SmsManager sms = SmsManager.getDefault();
-            sms.sendTextMessage(phoneNumber, null, message, null, null);
-            Toast.makeText(getApplicationContext(), "SMS Sent!", Toast.LENGTH_LONG).show();
+                sms.sendTextMessage(phoneNumber, null, message, null, null);
+            if(year_x == cal.get(Calendar.YEAR) && month_x == cal.get(Calendar.MONTH) && date_x == cal.get(Calendar.DAY_OF_MONTH) && hour_x ==cal.get(Calendar.HOUR_OF_DAY) && minute_x ==cal.get(Calendar.MINUTE)) {
+                Toast.makeText(getApplicationContext(), "SMS Sent!", Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "SMS failed to send", Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
     }
 
+    public DatePickerDialog.OnDateSetListener dpickerListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            year_x = year;
+            month_x = monthOfYear + 1;
+            date_x = dayOfMonth;
+            dateStr = month_x + "/" + date_x + "/" + year_x;
+            System.out.println(dateStr);
+
+        }
+    };
     // Dialog codes for date and time
     @Override
     protected Dialog onCreateDialog(int id) {
@@ -103,18 +115,9 @@ public class MainActivity extends AppCompatActivity {
         return null;
     }
 
-    private DatePickerDialog.OnDateSetListener dpickerListener = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            year_x = year;
-            month_x = monthOfYear + 1;
-            date_x = dayOfMonth;
-            dateStr = month_x + "/" + date_x + "/" + year_x;
-            System.out.println(dateStr);
-        }
-    };
 
-    private TimePickerDialog.OnTimeSetListener tpickerListener = new TimePickerDialog.OnTimeSetListener() {
+
+    public TimePickerDialog.OnTimeSetListener tpickerListener = new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             hour_x = hourOfDay;
